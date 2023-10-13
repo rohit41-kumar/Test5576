52bc80d1d7e2d38f756ed210eda5e9ca567bb54b
rohit41-kumar <115720489+rohit41-kumar@users.noreply.github.com>
2023-10-13 07:47:35 +0000

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -44,7 +44,7 @@ let package = Package(
         .binaryTarget(
             name: "Test5576SDK",
             url: "https://firebasestorage.googleapis.com/v0/b/chatapplication-bceea.appspot.com/o/0.0.2%2FTest5576SDK.xcframework.zip?alt=media&token=4537271c-4d85-4f4a-bc13-34a0502f6bef&_gl=1*a0skwq*_ga*MTUxNzU4NzA2OS4xNjQwNTExNzU5*_ga_CW55HF8NVT*MTY5NzE4Mjg2NC40LjEuMTY5NzE4Mjk5My40Ni4wLjA.",
-            checksum: "17bfd2b28323d98a0080836ed0d172be4c3622abb6984fc85a31b1eac957edfe"
+            checksum: "8b9b5641dfe3e2ce8d22ba86b055ebd19ad4e349d0ee3dcc6dc3a3be83e9edad"
         ),
         .target(
             name: "Test5576Target",
